700d8a899e79daf533cf4cf5059bab5eab71bb38
Ankit Kumar <ankit.k@adappt.ai>
2024-02-02 09:46:58 +0000

    updated README file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Image2TextAPI
+[![FastAPI Microservice for Django](https://static.codingforentrepreneurs.com/media/projects/fastapi-microservice-django/images/share/FastAPI_Microservice_for_Try_Django.jpg)](https://www.codingforentrepreneurs.com/projects/fastapi-microservice-django)
+
+
+The goal of this MicroService is to extract any and all text from images using a technique called OCR(Optical Character Recognition).
+
+Here's a list of the packages we have used in this project to accomplish this particular task:
+
+- FastAPI
+- Tesseract OCR
+- pytesseract
+- pre-commit
+- pytest
+- Gunicorn
+- Uvicorn
+- Requests
+- Docker
+- and more
+
+## Want to just run the app?
+Click below to deploy to DigitalOcean:
+
+
+[![Deploy to DO](https://www.deploytodo.com/do-btn-blue.svg)](https://cloud.digitalocean.com/apps/new?repo=https://github.com/ankitjaadoo/Image2TextAPI/tree/main)
--- app/test_production.py
+++ app/test_production.py
@@ -7,18 +7,17 @@ from app.main import BASE_DIR, UPLOAD_DIR, get_settings
 from PIL import Image, ImageChops
 import requests
 
-
-ENDPOINT = 'https://starfish-app-sbzj7.ondigitalocean.app/'
+ENDPOINT="https://fastapi-docker-l3j59.ondigitalocean.app/"
 
 def test_get_home():
-    response = requests.get("/") # requests.get("") #python requests
+    response = requests.get(ENDPOINT)
     assert response.text != "<h1>Hello world</h1>"
     assert response.status_code == 200
-    assert "text/html" in response.headers['content-type']
+    assert  "text/html" in response.headers['content-type']
 
 
 def test_invalid_file_upload_error():
-    response = requests.post("/") # requests.post("") # python requests
+    response = requests.post(ENDPOINT)
     assert response.status_code == 422
     assert  "application/json" in response.headers['content-type']
 
@@ -30,11 +29,12 @@ def test_prediction_upload_missing_headers():
             img = Image.open(path)
         except:
             img = None
-        response = requests.post("/",
+        response = requests.post(ENDPOINT,
             files={"file": open(path, 'rb')}
         )
         assert response.status_code == 401
-        
+
+
 def test_prediction_upload():
     img_saved_path = BASE_DIR / "images"
     settings = get_settings()
@@ -43,38 +43,14 @@ def test_prediction_upload():
             img = Image.open(path)
         except:
             img = None
-        response = requests.post("/", 
-                               files={"file": open(path,'rb')},
-                               headers={"Authorization": f"JWT {settings.app_auth_token_prod}"}
-                               ) 
+        response = requests.post(ENDPOINT,
+            files={"file": open(path, 'rb')},
+            headers={"Authorization": f"JWT {settings.app_auth_token_prod}"}
+        )
         if img is None:
             assert response.status_code == 400
         else:
-            #Checking whether a valid image is being returned
+            # Returning a valid image
             assert response.status_code == 200
             data = response.json()
-            print(data)
             assert len(data.keys()) == 2
-    #time.sleep(3)
-    shutil.rmtree(UPLOAD_DIR)
-
-def test_echo_upload():
-    img_saved_path = BASE_DIR / "images"
-    for path in img_saved_path.glob("*"):
-        try:
-            img = Image.open(path)
-        except:
-            img = None
-        response = requests.post("/img-echo/", files={"file": open(path,'rb')}) 
-        if img is None:
-            assert response.status_code == 400
-        else:
-            #Checking whether a valid image is being returned
-            assert response.status_code == 200
-            r_stream = io.BytesIO(response.content)
-            echo_img = Image.open(r_stream)
-            difference = ImageChops.difference(img, echo_img).getbbox()
-            print(difference)
-            assert difference is None
-    #time.sleep(3)
-    shutil.rmtree(UPLOAD_DIR)
